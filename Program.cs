01893c1f36a91153b1f0bd7a575bfc532cffc868
cspiller3589 <cspiller@crimson.ua.edu>
2024-09-30 08:08:39 -0500

    new file:   Program.cs 	new file:   mis-221-lab-4-cspiller3589.csproj 	new file:   obj/Debug/net7.0/.NETCoreApp,Version=v7.0.AssemblyAttributes.cs 	new file:   obj/Debug/net7.0/mis-221-lab-4-cspiller3589.AssemblyInfo.cs 	new file:   obj/Debug/net7.0/mis-221-lab-4-cspiller3589.AssemblyInfoInputs.cache 	new file:   obj/Debug/net7.0/mis-221-lab-4-cspiller3589.GeneratedMSBuildEditorConfig.editorconfig 	new file:   obj/Debug/net7.0/mis-221-lab-4-cspiller3589.GlobalUsings.g.cs 	new file:   obj/Debug/net7.0/mis-221-lab-4-cspiller3589.assets.cache 	new file:   obj/Debug/net7.0/mis-221-lab-4-cspiller3589.csproj.CoreCompileInputs.cache 	new file:   obj/Debug/net7.0/mis-221-lab-4-cspiller3589.csproj.FileListAbsolute.txt     new file:   obj/mis-221-lab-4-cspiller3589.csproj.nuget.dgspec.json 	new file:   obj/mis-221-lab-4-cspiller3589.csproj.nuget.g.props 	new file:   obj/mis-221-lab-4-cspiller3589.csproj.nuget.g.targets 	new file:   obj/project.assets.json 	new file:   obj/project.nuget.cache

---
+++ Program.cs
@@ -0,0 +1,89 @@
+﻿int badInputcounter = 0;
+
+bool showMenu = true;       //the main menu, everything that is not the correct menu numbers returns a error screen with new options.
+    Console.Clear();
+    Console.WriteLine("Welcome to Crimson Crust\n");
+
+    while(showMenu){
+    Console.WriteLine("Choose an option:\n1. Plain Pizza\n2. Cheese Pizza\n3. Pepperoni Pizza\n3. Exit");
+    string menuChoice = Console.ReadLine();
+    
+    if(menuChoice == "1"){
+        badInputcounter = 0;
+        PlainPizza();
+    }  
+    else if(menuChoice == "2"){
+        badInputcounter = 0;
+        CheesePizza();
+    }
+    else if (menuChoice == "3"){
+        badInputcounter = 0;
+        PepperoniPizza();
+    }
+    else if(menuChoice == "4"){
+        Console.Clear();
+        Console.WriteLine("Goodbye.");
+        showMenu = false;
+    }
+    else{
+        Console.Clear();
+        Console.WriteLine("Bad Input, Please Try Again (" + badInputcounter + ")\r\n");
+        badInputcounter++;
+    }
+}
+
+static void PlainPizza(){// The basic structure of this was helped in part because of my research on StackExchange. The majority of the process I already knew, however the general form and efficency I cannot take full credit for.
+        int rows = rand.Next(8, 13); // Random rows between 8 and 12
+        for (int i = rows; i > 0; i--)
+        {
+            Console.WriteLine(new string('*', i) + " ");
+        }
+    }
+
+static void CheesePizza(){// Modified version of Plain Pizza.
+        int rows = rand.Next(8, 13);
+        for (int i = rows; i > 0; i--)
+        {
+            if (i == rows || i == 1)  // Crust rows
+            {
+                Console.WriteLine(new string('*', i) + " ");
+            }
+            else  // Cheese
+            {
+                Console.WriteLine("*" + new string('#', i - 2) + "* ");
+            }
+        }
+    }
+
+static void DisplayPepperoniPizza()// Even more modified version of Cheese Pizza. Adds a random section for Pepperoni
+    {
+        int rows = rand.Next(8, 13);
+        for (int i = rows; i > 0; i--)
+        {
+            if (i == rows || i == 1)  // Crust rows
+            {
+                Console.WriteLine(new string('*', i) + " ");
+            }
+            else  // Cheese and pepperoni inside
+            {
+                char[] row = new char[i];
+                row[0] = '*';
+                row[i - 1] = '*';
+
+                for (int j = 1; j < i - 1; j++)  // Filling the row with cheese
+                {
+                    row[j] = '#';
+                }
+
+                // Randomly add pepperonis
+                int numPepperonis = rand.Next(1, i / 2);
+                for (int j = 0; j < numPepperonis; j++)
+                {
+                    int pepperoniPosition = rand.Next(1, i - 1);
+                    row[pepperoniPosition] = '[]';
+                }
+
+                Console.WriteLine(new string(row) + " ");
+            }
+        }
+    }
